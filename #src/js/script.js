69713ad002f2e812105a2b0a85078065d35e4cc7
Nikita <nikitaodessafreelance@gmail.com>
2020-10-20 20:12:04 +0300

    add block search

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    // @ @include("swiper-bundle.min.js");
+    // @@include("swiper-bundle.min.js");
     // @@include("some.js");
     // @@include('burger.js');
     const menuParents = document.querySelectorAll('.menu-page__parent');
@@ -14,4 +14,59 @@ document.addEventListener('DOMContentLoaded', () => {
             menuParent.classList.remove('_active');
         });
     });
+
+    const menuPageBurger = document.querySelector('.menu-page__burger');
+    menuPageBurger.addEventListener('click', () => {
+        const menuPageBody = document.querySelector('.menu-page__body');
+        menuPageBody.classList.toggle('_active');
+        menuPageBurger.classList.toggle('_active');
+    });
+
+    const searchBtn = document.querySelector('.search-page__select');
+    const categories = document.querySelector('.categories-search');
+    searchBtn.addEventListener('click', () => {
+        searchBtn.classList.toggle('_active');
+        categories.classList.toggle('_active');
+    });
+    let checkboxCheked = 0;
+    const checkboxCategories = document.querySelectorAll('.categories-search__checkbox');
+    checkboxCategories.forEach((element,index) => {
+        element.addEventListener('change', () => {
+            element.classList.toggle('_cheked');
+            if(element.classList.contains('_cheked')){
+                checkboxCheked++
+            }else{
+                checkboxCheked--
+            }
+            const checkedSpan = document.querySelector('.search-page__checkedspan');
+            const checkedSpanCategories = document.querySelector('.search-page__checkedspan span');
+            const defSpan = document.querySelector('.search-page__defspan');
+            if(checkboxCheked > 0){
+                checkedSpan.classList.add('_active');
+                defSpan.classList.add('_hide');
+                checkedSpanCategories.innerHTML = "";
+                checkedSpanCategories.insertAdjacentHTML('beforeend',checkboxCheked);
+            }else{
+                checkedSpan.classList.remove('_active');
+                defSpan.classList.remove('_hide');
+                checkedSpanCategories.innerHTML = "";
+            } 
+        });
+       
+    });
+    
+    //  slider main
+    const mainSlider = new Swiper('.mainslider__body', {
+        
+        slidesPerView: 1,
+        loop: true,
+        slideClass: 'mainslider__slide',
+        pagination: {
+          el: '.mainslider__dotts',
+          clickable: true,
+        },
+      
+     
+      })
+
 });
